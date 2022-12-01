b008959dc72c0669b6c5519157679f5c49824f79
Jan Meier <jan@meier.wtf>
2022-12-01 22:46:06 +0100

    Solves Day 1 Part 1 and 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,32 @@
+private fun createCaloriesSumArray(input: List<String>) : List<Int> {
+    val calorieGroups = mutableListOf(mutableListOf<Int>())
+    for(item in input) {
+        if (item.isEmpty()) {
+            calorieGroups.add(mutableListOf())
+        } else {
+            calorieGroups.last().add(item.toInt())
+        }
+    }
+    return calorieGroups.map { calGroup ->
+        calGroup.sumOf { it }
+    }
+}
+
+fun part1(input: List<String>): Int =
+     createCaloriesSumArray(input)
+         .max()
+
+fun part2(input: List<String>): Int =
+    createCaloriesSumArray(input)
+        .takeLast(3)
+        .sum()
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01_1_test")
+    check(part1(testInput) == 24000)
 
-    val input = readInput("Day01")
+    val input = readInput("Day01_1")
     println(part1(input))
     println(part2(input))
 }
